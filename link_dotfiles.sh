59698a6fb580019b70ee9cd0f88f5ec15e79d6c7
Steven Lai <slai@shutterstock.com>
2014-09-22 13:55:09 -0400

    added git stuff

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -2,6 +2,10 @@
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+
+curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
+
 CWD=$(pwd)
 for c in .bash_profile .tmux.conf .vimrc
 do
